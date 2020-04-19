3777b592be958f4e9d5639515bd7241c21319aa5
queebskeleton <queebskeleton@gmail.com>
2020-04-19 18:22:26 +0800

    Enable calendar capabilities for event resource
    Exposed:
    	- GET /api/events/search/findByExpectedStartBetween, a resource that
    	  requires 2 parameters, start (LocalDateTime format
    	  yyyy-MM-ddTHH:mm:ss), and end with same format as start. Returns
    	  array of events that has its expectedStart set between the 2
    	  parameters given.

--- src/main/java/com/gmail/queebskeleton/tpgofficeapp/repository/EventRepository.java
+++ src/main/java/com/gmail/queebskeleton/tpgofficeapp/repository/EventRepository.java
@@ -1,5 +1,7 @@
 package com.gmail.queebskeleton.tpgofficeapp.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -7,6 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.gmail.queebskeleton.tpgofficeapp.entity.Event;
@@ -15,10 +19,17 @@ import com.gmail.queebskeleton.tpgofficeapp.entity.Event;
 @RepositoryRestResource(collectionResourceRel = "events", path = "events")
 public interface EventRepository extends JpaRepository<Event, Long> {
 	
+	@RestResource(exported = true)
+	List<Event> findByExpectedStartBetween(
+			@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime first,
+			@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime second);
+	
+	@RestResource(exported = false)
 	@EntityGraph(attributePaths = { "userEventAttendances" })
 	@Query("SELECT e FROM Event e WHERE e.id = :id")
 	Optional<Event> findByIdFetchUserEventAttendances(@Param("id") long id);
 	
+	@RestResource(exported = false)
 	@EntityGraph(attributePaths = { "nonUserEventAttendances" })
 	@Query("SELECT e FROM Event e WHERE e.id = :id")
 	Optional<Event> findByIdFetchNonUserEventAttendances(@Param("id") long id);
